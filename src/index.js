851d2ba528a1b164fb2f56922842cdf023988e24
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-08-20 17:41:07 +0700

    Implement search location functionality

--- src/index.js
+++ src/index.js
@@ -13,9 +13,11 @@ import TemperatureHalf from "./images/temperature-half-solid.svg";
 import Wind from "./images/wind-solid.svg";
 
 const API_KEY = "e4cb62eba5da1b0dd44ba86650e28ccc";
-let currentTempUnit = "metric";
-let otherTempUnit = "imperial";
-let currentCity = "Bandung";
+let currentUnit = "metric";
+let otherUnit = "imperial";
+let currentLocation = "Bandung";
+
+let currentWeatherData = {};
 
 const container = document.createElement('div');
 
@@ -24,12 +26,12 @@ const mainStatus = document.createElement('div');
 const locationName = document.createElement('div');
 const dateTime = document.createElement('div');
 const mainTemp = document.createElement('div');
-const changeTempUnit = document.createElement('button');
+const changeUnitButton = document.createElement('button');
 const mainIcon = document.createElement('img');
 const formLocationContainer = document.createElement('div');
 const formLocation = document.createElement("form");
 const formLocationName = document.createElement("input");
-const formLocatoinSubmit = document.createElement("button");
+const formLocationSubmit = document.createElement("button");
 const locationNotFound = document.createElement('div');
 
 const otherInfoContainer = document.createElement('div');
@@ -54,12 +56,12 @@ mainStatus.classList.add('main-status');
 locationName.classList.add('location-name');
 dateTime.classList.add('date-time');
 mainTemp.classList.add('main-temp');
-changeTempUnit.classList.add('change-temp-unit');
+changeUnitButton.classList.add('change-unit-button');
 mainIcon.classList.add('main-icon');
 formLocationContainer.classList.add('form-location-container');
 formLocation.classList.add('form-location');
 formLocationName.classList.add('form-location-name');
-formLocatoinSubmit.classList.add('form-location-submit');
+formLocationSubmit.classList.add('form-location-submit');
 locationNotFound.classList.add('location-not-found');
 
 otherInfoContainer.classList.add('other-info-container');
@@ -79,10 +81,10 @@ windSpeedIcon.classList.add('windspeed-icon');
 footer.classList.add('footer');
 
 mainIcon.src = Cloud;
-locationName.textContent = currentCity;
-changeTempUnit.textContent = `Display ${otherTempUnit}`;
+locationName.textContent = currentLocation;
+changeUnitButton.textContent = `Display ${otherUnit}`;
 formLocationName.placeholder = "Search Location...";
-formLocatoinSubmit.textContent = "Search";
+formLocationSubmit.textContent = "Search";
 // locationNotFound.textContent = "Location not found. Search must be in the form of "City", "City, State" or "City, Country"."
 
 feelsLikeIcon.src = TemperatureHalf;
@@ -92,17 +94,17 @@ humidityText.textContent = "Humidity";
 windSpeedIcon.src = Wind;
 windSpeedText.textContent = "Wind Speed ";
 
-footer.innerHTML = "By yours truly, &nbsp; <a href='https://github.com/mraffia'> mraffia</a>";
+footer.innerHTML = "By yours truly,&nbsp;<a href='https://github.com/mraffia'> mraffia</a>";
 
 mainInfoContainer.appendChild(mainStatus);
 mainInfoContainer.appendChild(locationName);
 mainInfoContainer.appendChild(dateTime);
 mainInfoContainer.appendChild(mainTemp);
-mainInfoContainer.appendChild(changeTempUnit);
+mainInfoContainer.appendChild(changeUnitButton);
 mainInfoContainer.appendChild(mainIcon);
+formLocation.appendChild(formLocationName);
+formLocation.appendChild(formLocationSubmit);
 formLocationContainer.appendChild(formLocation);
-formLocationContainer.appendChild(formLocationName);
-formLocationContainer.appendChild(formLocatoinSubmit);
 mainInfoContainer.appendChild(formLocationContainer);
 mainInfoContainer.appendChild(locationNotFound);
 
@@ -123,9 +125,9 @@ container.appendChild(mainInfoContainer);
 container.appendChild(otherInfoContainer);
 container.appendChild(footer);
 
-async function getWeatherData() {
+async function getWeatherData(location, unit) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${API_KEY}&units=${currentTempUnit}`, {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=${unit}`, {
             mode: 'cors'
         })
         const weatherData = await response.json();
@@ -142,7 +144,7 @@ async function getWeatherData() {
 }
 
 function filterWeatherData(data) {
-    let currentWeatherData = {};
+    let kiloPerHour = Number(data["wind"]["speed"]) * 3.6;
 
     currentWeatherData["dt"] = data["dt"];
     currentWeatherData["main"] = { "feels_like": data["main"]["feels_like"] };
@@ -158,16 +160,77 @@ function filterWeatherData(data) {
 
 function populateWeatherData(data) {
     let currentDate = new Date(data["dt"]);
+    let tempUnit;
+    let speedUnit;
+
+    if(currentUnit === "metric") {
+        tempUnit = "°C";
+        speedUnit = "km/h";
+    } else {
+        tempUnit = "°F";
+        speedUnit = "mph";
+    }
 
     locationName.textContent = data["name"];
     dateTime.textContent = currentDate.toString();
-    mainTemp.textContent = Math.round(data["main"]["temp"]) + "°C";
+    mainTemp.textContent = Math.round(data["main"]["temp"]) + tempUnit;
 
-    feelsLikeValue.textContent = Math.round(data["main"]["feels_like"]) + "°C";
+    feelsLikeValue.textContent = Math.round(data["main"]["feels_like"]) + tempUnit;
     humidityValue.textContent = data["main"]["humidity"] + "%";
-    windSpeedValue.textContent = data["wind"]["speed"] + "m/s";
+    windSpeedValue.textContent = Number(data["wind"]["speed"]).toFixed(2) + speedUnit;
+
+    changeUnitButton.textContent = `Display ${otherUnit}`;
 }
 
-getWeatherData();
+function cToF(celsius) {
+    let cTemp = celsius;
+    let cToFahr = cTemp * 9 / 5 + 32;
+    return cToFahr;
+}
+
+function fToC(fahrenheit) {
+    let fTemp = fahrenheit;
+    let fToCel = (fTemp - 32) * 5 / 9;
+    return fToCel;
+}
+
+function changeUnit() {
+    let temp = currentWeatherData["main"]["temp"];
+    let speed = currentWeatherData["wind"]["speed"];
+
+    let newTemp;
+    let newSpeed;
+
+    if (currentUnit === "metric") {
+        newTemp = cToF(temp);
+        newSpeed = speed / 1.609;
+        currentUnit = "imperial";
+        otherUnit = "metric";
+    } else {
+        newTemp = fToC(temp);
+        newSpeed = speed * 1.609;
+        currentUnit = "metric";
+        otherUnit = "imperial";
+    }
+
+    currentWeatherData["main"]["feels_like"] = newTemp;
+    currentWeatherData["main"]["temp"] = newTemp;
+    currentWeatherData["wind"]["speed"] = newSpeed;
+
+    populateWeatherData(currentWeatherData);
+}
+
+changeUnitButton.addEventListener('click', () => {
+    changeUnit();
+});
+
+formLocation.addEventListener('submit', (e) => {
+    e.preventDefault();
+    currentLocation = e.target[0].value;
+    e.target.reset();
+    getWeatherData(currentLocation, currentUnit);
+});
+
+getWeatherData(currentLocation, currentUnit);
 
 document.body.appendChild(container);
